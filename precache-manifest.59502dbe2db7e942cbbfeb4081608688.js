f65a8fa54d5f9711cab0f633066c95c67520236e
Tim Westhoff <tim.westhoff@siemens.com>
2020-04-29 02:46:25 +0200

    Updates

---
+++ precache-manifest.59502dbe2db7e942cbbfeb4081608688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d976f04dfd3a3d96b7a057e4d959ac5",
+    "url": "/wl4-bis/index.html"
+  },
+  {
+    "revision": "35c42d97ec376554cf6f",
+    "url": "/wl4-bis/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "af27af9b5e96238e4d24",
+    "url": "/wl4-bis/static/js/2.9cd5cfe1.chunk.js"
+  },
+  {
+    "revision": "3c4fca76e388cecffa735d37fc12ae2b",
+    "url": "/wl4-bis/static/js/2.9cd5cfe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c42d97ec376554cf6f",
+    "url": "/wl4-bis/static/js/main.ec1f7ce0.chunk.js"
+  },
+  {
+    "revision": "5b3c79f8b396207b2d6f",
+    "url": "/wl4-bis/static/js/runtime-main.f4c4c5cb.js"
+  },
+  {
+    "revision": "3ff61d7f75b187a6e24a8e696edbbbb5",
+    "url": "/wl4-bis/static/media/thu-logo.3ff61d7f.png"
+  }
+]);
